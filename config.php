9bca043ab03a8ccd24de389e69ec08b2d6b30620
mhasanfuadd22 <76463319+mhasanfuadd22@users.noreply.github.com>
2021-01-20 16:50:09 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php
+
+// untuk menambahkan tinggal copy yang ada terus paste di bawahnya, 
+// ganti kode negara sesuai countrycode.org
+
+$linkOffer = array( 
+    "ALL" => "https://securecd-smrt.com/smartlink/?a=121519&sm=5048&mt=19&s1=KEWEK&s2=KEWEK", // Yang ini jangan dihapus, sebagai default
+    "BE" => "https://vozkn.shewantyou.com/c/da57dc555e50572d?s1=74105&s2=1212594&s3=xnxx&click_id=TERPESONA&j1=1&j3=1",
+    "DE" => "https://vozkn.shewantyou.com/c/da57dc555e50572d?s1=74105&s2=1212594&s3=xnxx&click_id=TERPESONA&j1=1&j3=1",
+    "FR" => "https://vozkn.shewantyou.com/c/da57dc555e50572d?s1=74105&s2=1212594&s3=xnxx&click_id=TERPESONA&j1=1&j3=1",
+    "LU" => "https://vozkn.shewantyou.com/c/da57dc555e50572d?s1=74105&s2=1212594&s3=xnxx&click_id=TERPESONA&j1=1&j3=1",
+    "NL" => "https://vozkn.shewantyou.com/c/da57dc555e50572d?s1=74105&s2=1212594&s3=xnxx&click_id=TERPESONA&j1=1&j3=1",
+    "CH" => "https://vozkn.shewantyou.com/c/da57dc555e50572d?s1=74105&s2=1212594&s3=xnxx&click_id=TERPESONA&j1=1&j3=1",
+    "AT" => "https://vozkn.shewantyou.com/c/da57dc555e50572d?s1=74105&s2=1212594&s3=xnxx&click_id=TERPESONA&j1=1&j3=1",
+);
